392e8207888fefcf2575adbfb2c34452e06da8af
alees <nprolol8@gmail.com>
2024-02-28 13:54:01 +0300

    chore: fix after rewiew

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,18 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { User } from "../ts/mok_interfaces.ts";
+import React, {useEffect, useState} from 'react';
+import {User} from "../ts/mok_interfaces.ts";
 import Card from "./Card.tsx";
-import '../assets/styles/animations.scss';
 import './CardList.scss'
 
 interface CardListProps {
   items: User[];
 }
 
-const CardList: React.FC<CardListProps> = ({ items }) => {
+const CardList: React.FC<CardListProps> = ({items}) => {
   const batchSize = 20;
   const [displayedItems, setDisplayedItems] = useState<User[]>([]);
   const [loaded, setLoaded] = useState(0);
-  const [showButton, setShowButton] = useState(false);
+  const [showButton, setShowButton] = useState(true);
 
   useEffect(() => {
     setDisplayedItems(items.slice(0, batchSize));
@@ -44,14 +43,19 @@ const CardList: React.FC<CardListProps> = ({ items }) => {
   }, [loaded, showButton, displayedItems.length, items]);
 
   return (
-    <div className='list'>
+    <ul className='list'>
       {displayedItems.map((item, index) => (
-        <Card key={index} name={item.name} surname={item.surname} />
+        <Card key={index} name={item.name} surname={item.surname}/>
       ))}
       {showButton && loaded < items.length && (
-        <button className='list__button' onClick={loadMoreItems}>Показать еще</button>
+        <button
+          type='button'
+          className='list__button'
+          onClick={loadMoreItems}>
+          Показать еще
+        </button>
       )}
-    </div>
+    </ul>
   );
 }
 
